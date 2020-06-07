56e9b1921ff248f43fac7de5850a6d672ff57b6c
Nicholas Leite <nicklleite@gmail.com>
2020-06-07 04:43:18 -0300

    SEO e Analytics

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
-    // https://api.exchangeratesapi.io/2010-01-12
-
-    get_current_quota("https://api.exchangeratesapi.io/latest?base=USD&symbols=BRL");
-
     $("#pesquisar_historico").on('click', function(e) {
         e.preventDefault();
 
         var date = $("#data_historico").val();
         get_quota_by_date("https://api.exchangeratesapi.io/" + date, date);
+        get_current_quota("https://api.exchangeratesapi.io/latest?base=USD&symbols=BRL");
     });
 });
 
@@ -19,6 +16,7 @@ function get_current_quota(url) {
         success: function(data) {
             var formated = new Intl.NumberFormat('pt-BR', {maximumSignificantDigits: 3}).format(data['rates']['BRL']);
 
+            $("#current_quota_president").attr('src', 'assets/images/bolsadecoco.png');
             $("#current_quota").text("R$" + formated);
         }
     });
@@ -35,9 +33,9 @@ function get_quota_by_date(url, date) {
             var temp = date.split('-');
             console.log(parseInt(temp[0]));
 
-            if (parseInt(temp[0]) <= 2011) {
+            if (parseInt(temp[0]) <= 2010) {
                 $("#old_quota_president").attr('src', 'assets/images/lulalindo.png');
-            } else if (parseInt(temp[0]) >= 2012 && parseInt(temp[0]) <= 2016) {
+            } else if (parseInt(temp[0]) >= 2011 && parseInt(temp[0]) <= 2016) {
                 $("#old_quota_president").attr('src', 'assets/images/dilmae.png');
             } else if (parseInt(temp[0]) >= 2017 && parseInt(temp[0]) <= 2018) {
                 $("#old_quota_president").attr('src', 'assets/images/vampiraodacabecona.png');
