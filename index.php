2b40856db2642768885c948eabf21b8e82429c44
Nicholas Leite <nicklleite@gmail.com>
2021-10-22 15:47:21 -0300

    For Heroku!

---
+++ index.php
@@ -0,0 +1,120 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-40094593-5"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-40094593-5');
+        </script>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Não Compre Dólar Agora! | Auto flagelamento mental. | BETA</title>
+
+        <!-- Primary Meta Tags -->
+        <title>Não Compre Dólar Agora! | Auto flagelamento mental | BETA</title>
+        <meta name="title" content="Não Compre Dólar Agora! | Auto flagelamento mental | BETA">
+        <meta name="description" content="Venha conosco em uma jornada no tempo, onde o mundo era um lugar completamente diferente. Nossa primeira e única parada é a cotação do dólar.">
+        <meta name="keywords" content="cotação do dólar, dólar, cotação, passado">
+
+        <!-- Open Graph / Facebook -->
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="http://naocompredolar.com/">
+        <meta property="og:title" content="Não Compre Dólar Agora! | Auto flagelamento mental | BETA">
+        <meta property="og:description" content="Venha conosco em uma jornada no tempo, onde o mundo era um lugar completamente diferente. Nossa primeira e única parada é a cotação do dólar.">
+        <meta property="og:image" content="assets/images/seo.png">
+
+        <!-- Twitter -->
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:url" content="http://naocompredolar.com/">
+        <meta property="twitter:title" content="Não Compre Dólar Agora! | Auto flagelamento mental | BETA">
+        <meta property="twitter:description" content="Venha conosco em uma jornada no tempo, onde o mundo era um lugar completamente diferente. Nossa primeira e única parada é a cotação do dólar.">
+        <meta property="twitter:image" content="assets/images/seo.png">
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/css/main.css">
+    </head>
+    <body>
+        <div class="container-fluid no-gutter bg-success">
+            <div class="container">
+                <div class="jumbotron bg-transparent no-border-radius text-white text-center">
+                    <h1 class="display-3 font-weight-bold">NCDA!</h1>
+                    <small>Não Compre Dólar Agora!</small>
+                </div>
+            </div>
+        </div>
+
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-md-6 col-xs-12">
+                    <blockquote class="twitter-tweet" style="margin: 0 auto;">
+                        <p lang="pt" dir="ltr">Não compre dólar agora!</p>
+                        &mdash; Eduardo Bolsonaro🇧🇷 (@BolsonaroSP)
+                        <a href="https://twitter.com/BolsonaroSP/status/720586690360909824?ref_src=twsrc%5Etfw">April 14, 2016</a>
+                    </blockquote>
+                </div>
+                <div class="col-md-6 col-xs-12">
+                    <blockquote class="twitter-tweet">
+                        <p lang="pt" dir="ltr">- Brasil acima de tudo!</p>
+                        &mdash; Jair M. Bolsonaro (@jairbolsonaro)
+                        <a href="https://twitter.com/jairbolsonaro/status/1263940112414580739?ref_src=twsrc%5Etfw">May 22, 2020</a>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <hr class="my-4">
+        </div>
+
+        <div class="container">
+            <h1 class="font-family-dancing text-center">Bora sofrer juntos?</h1>
+            <hr>
+            <form id="form-search-dollar">
+                <div class="form-row justify-content-md-center">
+                    <div class="col-md-4 col-xs-6">
+                        <input type="date" class="form-control" id="data_historico" min="2008-01-02" max="2018-12-31">
+                    </div>
+                    <div class="col-md-2 col-xs-6">
+                        <button class="btn btn-block btn-primary mb-2" id="pesquisar_historico">Pesquisar</button>
+                    </div>
+                    <div class="col-12 text-center">
+                        <small>Obs.: Infelizmente ainda não conseguimos dados de antes de 2008. ☹☹</small>
+                    </div>
+                </div>
+            </form>
+
+            <div class="row mt-5">
+                <div class="col-md-6 col-xs-12 text-center" id="wrap_old_quota">
+                    <h3 class="font-family-inter text-success">Antes</h3>
+                    <span class="font-weight-bold display-2 text-success" id="old_quota"></span><br>
+                    <img src="" class="img-fluid mt-3 mb-5" id="old_quota_president">
+                </div>
+                <div class="col-md-6 col-xs-12 text-center">
+                    <h3 class="font-family-inter text-success">Hoje</h3>
+                    <span class="font-weight-bold display-2 text-success" id="current_quota"></span><br>
+                    <img src="" class="img-fluid mt-3 mb-5" id="current_quota_president">
+                </div>
+            </div>
+        </div>
+
+        <footer class="mt-5 bg-success">
+            <div class="container text-center text-white">
+                <p>O conteúdo deste site é puramente humorístico e não reflete a opinião política dos seus criadores.</p>
+                <p>&copy; 2020</p>
+            </div>
+        </footer>
+        
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/js/all.min.js" integrity="sha256-+Q/z/qVOexByW1Wpv81lTLvntnZQVYppIL1lBdhtIq0=" crossorigin="anonymous"></script>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <script src="assets/js/main.js"></script>
+    </body>
+</html>
